c722e233f3a52ac238a03f776599b613a4da1f60
raziqali0306 <raziqali0306@gmai.com>
2022-07-13 17:25:11 +0530

    modifies basepath and assetprefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/nextjs-pages',
-  assetPrefix: '/nextjs-pages',
+  basePath: '/myportfolio/',
+  assetPrefix: '/https://raziqali0306.github.io/myportfolio/',
 }
 
 module.exports = nextConfig
